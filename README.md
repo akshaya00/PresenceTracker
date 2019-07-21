1c40e933d60e9d25f2da76b64792e2a710877e88
Akshaya Kumar <percyakki@gmail.com>
2019-07-21 17:04:35 +0530

    module initializers functions 1. to create folder for school 2. to generate encodings.pickle 3. to create folder for classroom

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ats
-attendance tracking system
+    Attendance Tracking System
+
+1. The system will automatically update attendances fo the students it is trained to
+2. Will provide a automated onboarding and editing abilities
+3. Will expose api endpoints to all functionalities
+
+ 
+ The basic directory structure of the project will be as follows:
+
+
---
+++ initializers.py
@@ -0,0 +1,64 @@
+"""
+This module will initialize folders for school, classes and students
+"""
+
+
+import pathlib
+import os
+import datetime
+
+#Fucntion to initialize a school
+def createFolderForSchool(id_generated_for_school):
+    #creates a folder in the data folder
+    pathlib.Path('data/'+str(id_generated_for_school)).mkdir(parents=False, exist_ok=True)
+
+    #creates a folder in encodings folder
+    pathlib.Path('encodings/'+ str(id_generated_for_school)).mkdir(parents=False, exist_ok=True)
+
+"""
+test scrip for createFolderForSchool
+
+createFolderForSchool('strw239')
+"""
+
+
+#Fuction to create encodings file
+def createEncodingsFile(path):
+    file = pathlib.Path(path+'/'+'encodings.pickle')
+    #create a version of current pickle file
+    if file.exists():
+        os.rename(path+'/'+'encodings.pickle',path+'/'+'encodings '+str(datetime.datetime.now())+'.pickle')
+    encodings_file = open(path+'/'+'encodings.pickle', "a")
+    encodings_file.close()
+
+"""
+test scrip for createEncodingsFile
+
+createEncodingsFile('strw239')
+"""
+
+
+
+#Function to add a classroom to the school
+def addFolderForClassroom(id_generated_for_school,standard,section,number_of_students):
+    # creates a folder in the folder for school
+    pathlib.Path('data/' + str(id_generated_for_school)+'/'+str(standard)+'-'+str(section)).mkdir(parents=False, exist_ok=True)
+
+    #create folders for all students
+    for iterator in range(number_of_students):
+        pathlib.Path('data/' + str(id_generated_for_school) + '/' + str(standard) + '-' + str(section)+'/'+str(iterator)).mkdir(
+            parents=False, exist_ok=True)
+
+    # creates a folder in encodings folder
+    pathlib.Path('encodings/' + str(id_generated_for_school) + '/' + str(standard) + '-' + str(section)).mkdir(parents=False,
+                                                                                                          exist_ok=True)
+    #generate a encodings file
+    createEncodingsFile('encodings/' + str(id_generated_for_school) + '/' + str(standard) + '-' + str(section))
+
+"""
+test script for addFolderForClassroom
+
+addFolderForClassroom('strw239',7,'C',4)
+"""
+
+
