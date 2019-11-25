8b0535e3c53bdc4c09fe64a38b7ad62b72435531
Akshaya Kumar <percyakki@gmail.com>
2019-11-26 01:21:46 +0530

    1. rest api to add client request to process facial data 2. added support submitting processed data to output server/database 3. added multiprocessing for facial encoding

--- APIservice/embeddingGenerator.py
+++ APIservice/embeddingGenerator.py
@@ -3,48 +3,64 @@ import urllib
 import urllib.request
 import cv2
 import numpy as np
-import asyncio
 import time
+import multiprocessing
+import json
+
 
 class EmbeddingGenerator:
 
-    def __init__(self):
+    def printQueue(self):
+        while not self.inputQueue.empty():
+            print("thread "+str(self.threadNum)+" path "+str(self.inputQueue.get()))
+            time.sleep(0.01)
+
+    def getFaceData(self):
+        print("thread "+str(self.threadNum)+" started")
+        while True:
+            if not self.inputQueue.empty():
+                queueElement = self.inputQueue.get()
+                deviceId = queueElement['deviceId']
+                videoPath = queueElement['path']
+                video = cv2.VideoCapture(videoPath)
+                #video =cv2.VideoCapture(0)
+                ret, frame = video.read()
+
+                while ret:
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                    cv2.imshow('frame',frame)
+                    faces = self.model.get(frame)
+
+                    for idx, face in enumerate(faces):
+                        #print("bbox = "+str(type(face.bbox))+" landmark ="+str(type(face.landmark))+" det_score = "+str(type(face.det_score))+" embedding ="+str(type(face.embedding))+" gender ="+ str(type(face.gender))+" age = "+str(type(face.age))+" normed_embedding="+str(type(face.normed_embedding))+" embedding_norm =" +str(type(face.embedding_norm)))
+                        data = {}
+                        data['deviceId'] = deviceId
+                        data['time'] = video.get(cv2.CAP_PROP_POS_MSEC)
+                        #self.outputQueue.put(face)
+                        data['faceIndex']=str(idx)
+                        data['bbox'] = face.bbox.tolist()
+                        data['landmark'] = face.landmark.tolist()
+                        data['det_score'] = str(face.det_score)
+                        data['embedding']= face.embedding.tolist()
+                        data['gender'] = str(face.gender)
+                        data['age']= str(face.age)
+                        data['normed_embedding'] = face.normed_embedding.tolist()
+                        data['embedding_norm']=str(face.embedding_norm)
+                        self.outputQueue.put(json.dumps(data))
+                    ret, frame = video.read()
+                video.release()
+                cv2.destroyAllWindows()
+
+    def __init__(self, threadnum, inputQueue, outputQueue):
+        self.threadNum = threadnum
+        self.inputQueue = inputQueue
+        self.outputQueue = outputQueue
+        #self.printQueue()
         self.model = insightface.app.FaceAnalysis()
         self.ctx_id = 0
         self.model.prepare(ctx_id=self.ctx_id, nms=0.4)
-        self.isBusy = False
-
-    def addFaceData(self, image, lock):
-        lock.acquire()
-        print("Is busy to add : " + str(self.isBusy))
-        x = []
-        if self.isBusy:
-            x = [False, None]
-        else:
-            self.isBusy = True
-            x = [True, self.getFaceData(image)]
-        lock.release()
-        return x
-
-    async def getFaceData(self, image):
-        faces = self.model.get(image)
-
-        for idx, face in enumerate(faces):
-            print("Face [%d]:" % idx)
-            """print("\tage:%d" % (face.age))
-            gender = 'Male'
-            if face.gender == 0:
-                gender = 'Female'
-            print("\tgender:%s" % (gender))
-            print("\tembedding shape:%s" % face.embedding.shape)
-            #	print(face.embedding)
-            print("\tbbox:%s" % (face.bbox.astype(np.int).flatten()))
-            print("\tlandmark:%s" % (face.landmark.astype(np.int).flatten()))
-            print("")
-            """
-        # print ("Result from getFaceData, isBusy " + str(self.isBusy))
-        self.isBusy = False
-        return True
-
-    def generatorIsBusy(self):
-        return self.isBusy
+        #p1= multiprocessing.Process(target=self.getFaceData)
+        print("starting thread "+str(threadnum))
+        #p1.start()
+        self.getFaceData()
